b4c0012a075f8c9dc66ec138d137ee0790c429c0
alexmwihia054-cyber <alexmwihia054@gmail.com>
2025-09-23 15:01:21 +0300

    Add files via upload

---
+++ units for this semester.c
@@ -0,0 +1,23 @@
+//simple c program (units for this semester)
+/*
+Name ;Alex Mwangi Mwihia 
+Reg;CT100\G\26203\25
+description;units for this semester program
+*/
+#include <stdio.h>
+// main function
+int main() {
+        printf("************************ \n");
+        printf("Units for the semester \n");
+        printf("************************ \n");
+        printf("1.Analogue electronics \n");
+        printf("2.Introduction to programming and algorithms \n");
+        printf("3.Computer systems and organization \n");
+        printf("4.Introduction to computer application \n");
+        printf("5.Essentials of accounting \n");
+        printf("6.Mathematics for science \n");
+        printf("7.Fundamentals of ICT \n");
+        printf("8.Communication and information literacy \n");
+
+    return 0;
+}
